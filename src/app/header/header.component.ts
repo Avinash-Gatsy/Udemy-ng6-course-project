e2c02ad63e08bfd19a3d955be92511e1e07fd438
Avinash <avinashg12291992@gmail.com>
2018-07-30 21:06:23 +0530

    first commit

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 
 @Component({
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
     templateUrl: './header.component.html',
 })
 export class HeaderComponent {
-
+  // @Output() decorator to make the event listenable from parent scope, in this case app.component.html
+  @Output() featureSelected = new EventEmitter<string>();
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,13 +8,20 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('Chicken Biryani',
+    new Recipe('Chicken Biryani 1',
+      'Chicken biryani is a simple dish of chicken and rice.',
+      'http://www.authenticroyal.com/wp-content/uploads/2017/07/Royal_ChickenProvencal_Horizontal-square.jpg'),
+    new Recipe('Chicken Biryani 2',
       'Chicken biryani is a simple dish of chicken and rice.',
       'http://www.authenticroyal.com/wp-content/uploads/2017/07/Royal_ChickenProvencal_Horizontal-square.jpg')
   ];
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   constructor() { }
 
   ngOnInit() {
+  }
+  onRecipeSelected(recipe: Recipe) {
+  this.recipeWasSelected.emit(recipe);
   }
 
 }
